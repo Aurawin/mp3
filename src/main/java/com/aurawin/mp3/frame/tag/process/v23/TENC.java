be884c2057a3ea3ff1b40f2725819d2e86b50b9b
atbrunner <atbrunner@aurawin.com>
2018-04-06 10:40:45 -0500

    TDAT, TDLY, TENC, TEXT, TFLT, TIME, TIT1, TIT2, TIT3, TKEY, TLAN tags

---
+++ src/main/java/com/aurawin/mp3/frame/tag/process/v23/TENC.java
@@ -0,0 +1,31 @@
+package com.aurawin.mp3.frame.tag.process.v23;
+
+import com.aurawin.core.stream.MemoryStream;
+import com.aurawin.mp3.frame.Payload;
+import com.aurawin.mp3.frame.tag.process.Process;
+import com.aurawin.mp3.frame.tag.process.Processor;
+
+import static com.aurawin.mp3.frame.Kind.fEncodedBy;
+import static com.aurawin.mp3.frame.Kind.fPlaylistDelay;
+
+public class TENC extends Processor implements Process {
+    @Override
+    public boolean process( MemoryStream Stream){
+        Owner.Length=Owner.Reader.TagFrame.Header.Length;
+        Owner.StreamStart=Stream.Position;
+        Owner.Data=Owner.pldText;
+        Owner.Reader.TagFrame.Kind=fEncodedBy;
+
+        Owner.pldText.Encoding= Owner.pldText.Encoding.fromByte(Stream.readByte());
+        Owner.pldText.Data = Stream.readStringUntil((byte)0, Stream.Position, Owner.pldText.Encoding.toEncoding());
+
+        return true;
+    }
+    @Override
+    public void Reset(){
+
+    }
+    public TENC(Payload owner) {
+        super("TENC",owner);
+    }
+}
---
+++ src/main/java/com/aurawin/mp3/frame/tag/process/v23/TKEY.java
@@ -0,0 +1,31 @@
+package com.aurawin.mp3.frame.tag.process.v23;
+
+import com.aurawin.core.stream.MemoryStream;
+import com.aurawin.mp3.frame.Payload;
+import com.aurawin.mp3.frame.tag.process.Process;
+import com.aurawin.mp3.frame.tag.process.Processor;
+
+import static com.aurawin.mp3.frame.Kind.fInitialKey;
+import static com.aurawin.mp3.frame.Kind.fSubTitleDescription;
+
+public class TKEY extends Processor implements Process {
+    @Override
+    public boolean process( MemoryStream Stream){
+        Owner.Length=Owner.Reader.TagFrame.Header.Length;
+        Owner.StreamStart=Stream.Position;
+        Owner.Data=Owner.pldText;
+        Owner.Reader.TagFrame.Kind=fInitialKey;
+
+        Owner.pldText.Encoding= Owner.pldText.Encoding.fromByte(Stream.readByte());
+        Owner.pldText.Data = Stream.readStringUntil((byte)0, Stream.Position, Owner.pldText.Encoding.toEncoding());
+
+        return true;
+    }
+    @Override
+    public void Reset(){
+
+    }
+    public TKEY(Payload owner) {
+        super("TKEY",owner);
+    }
+}
